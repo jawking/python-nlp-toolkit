ea90c9dcfc1ae0753c8fc46aae93ac1cd20509d7
jawking <126416933+jawking@users.noreply.github.com>
2020-09-14 19:17:25 +0000

    Google Analytics

--- docs/conf.py
+++ docs/conf.py
@@ -141,4 +141,18 @@ else:
 # of the sidebar.
 # html_logo = ""
 
-# The nam
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+# html_favicon = None
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+# html_last_updated_fmt = '%b %d, %Y'
+
+# If true, SmartyPants will be used to conv
