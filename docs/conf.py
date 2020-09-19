df7c88971752a8f961561b98018dea8191ed9e7f
jawking <126416933+jawking@users.noreply.github.com>
2020-09-19 19:51:31 +0000

    Translated using Translate.Akeeba (Portuguese (Brazil))

--- docs/conf.py
+++ docs/conf.py
@@ -222,4 +222,25 @@ latex_documents = [
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-# latex_use_parts = Fals
+# latex_use_parts = False
+
+# If true, show page references after internal links.
+# latex_show_pagerefs = False
+
+# If true, show URL addresses after external links.
+# latex_show_urls = False
+
+# Documents to append as an appendix to all manuals.
+# latex_appendices = []
+
+# If false, no module index is generated.
+# latex_domain_indices = True
+
+# -- External mapping ------------------------------------------------------------
+python_version = '.'.join(map(str, sys.version_info[0:2]))
+intersphinx_mapping = {
+    'sphinx': ('http://sphinx.pocoo.org', None),
+    'python': ('http://docs.python.org/' + python_version, None),
+    'matplotlib': ('http://matplotlib.sourceforge.net', None),
+    'numpy': ('http://docs.scipy.org/doc/numpy', None),
+    'sklearn': ('http://sciki
