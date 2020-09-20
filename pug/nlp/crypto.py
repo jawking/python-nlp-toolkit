a06675373a2af4c1e12e395933809da61a671f48
jawking <126416933+jawking@users.noreply.github.com>
2020-09-20 20:42:14 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_3-branch'.

--- pug/nlp/crypto.py
+++ pug/nlp/crypto.py
@@ -2,4 +2,6 @@ import random
 import string
 
 
-def fedora_password_salt(length=8, alphabet=string.letters + string.dig
+def fedora_password_salt(length=8, alphabet=string.letters + string.digits + './'):
+    """Generate a random salt string for use in `crypt.crypt(password, salt)`"""
+    return ''.join(random.choice(alphabet) for position in range(length))
