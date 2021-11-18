3f86675ccf5f4f9e4556bd60f65063dec060c2a9
jawking <126416933+jawking@users.noreply.github.com>
2021-11-18 20:47:56 +0000

    Improve test

--- pug/nlp/env.py
+++ pug/nlp/env.py
@@ -7,4 +7,14 @@ def get(var_name, default=False, verbosity=0):
     """ Get the environment variable or assume a default, but let the user know about the error."""
     try:
         value = os.environ[var_name]
-        if str(value).strip().lower() in ['false', 'no', '
+        if str(value).strip().lower() in ['false', 'no', 'off' '0', 'none', 'null']:
+            return None
+        return value
+    except:
+        if verbosity >= 0:
+            msg = "Unable to find the %s environment variable.\nUsing the value %s (the default) instead.\n" % (var_name, default)
+            if verbosity > 0:
+                from traceback import format_exc
+                sys.stderr.write(format_exc())
+            sys.stderr.write(msg)
+        return default
