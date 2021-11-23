793d2c29b00fb4fdeea64546288d319233317ee2
jawking <126416933+jawking@users.noreply.github.com>
2021-11-23 13:49:03 +0000

    image changes

--- pug/nlp/futil.py
+++ pug/nlp/futil.py
@@ -16,4 +16,26 @@ import os
 import datetime
 import warnings
 import subprocess
-from collections import Mappin
+from collections import Mapping
+import errno
+
+
+def walk_level(path, level=1):
+    """Like os.walk, but takes `level` kwarg that indicates how deep the recursion will go.
+
+    Notes:
+      TODO: refactor `level`->`depth`
+
+    References:
+      http://stackoverflow.com/a/234329/623735
+
+    Args:
+     path (str):  Root path to begin file tree traversal (walk)
+      level (int, optional): Depth of file tree to halt recursion at.
+        None = full recursion to as deep as it goes
+        0 = nonrecursive, just provide a list of files at the root level of the tree
+        1 = one level of depth deeper in the tree
+
+    Examples:
+      >>> root = os.path.dirname(__file__)
+      >>> all((os.path.join
