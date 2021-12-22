c5c1a9c92bae50e3855d199c0af5858a3a3c36ee
jawking <126416933+jawking@users.noreply.github.com>
2021-12-22 07:02:18 +0000

    Update requirements_dev.txt

--- pug/nlp/penn_treebank_tokenizer.py
+++ pug/nlp/penn_treebank_tokenizer.py
@@ -9,4 +9,16 @@
 # the following conditions:
 #
 # The above copyright notice and this permission notice shall be included
-# in all copies o
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+"""Penn Treebank tokenizer, adapted from `nltk.tokenize.treebank.py`, which
+in turn is adapted from an infamous sed script by Robert McIntyre.
