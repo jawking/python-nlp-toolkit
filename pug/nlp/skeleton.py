c734806e3681170ece10c7ac1a87076d8259f183
jawking <126416933+jawking@users.noreply.github.com>
2021-12-23 22:17:40 +0000

    Images added

--- pug/nlp/skeleton.py
+++ pug/nlp/skeleton.py
@@ -3,4 +3,33 @@
 """
 This is a skeleton file that can serve as a starting point for a Python
 console script. To run this script uncomment the following line in the
-entry_poin
+entry_points section in setup.cfg:
+
+    console_scripts =
+     fibonacci = nlp.skeleton:run
+
+Then run `python setup.py install` which will install the command `fibonacci`
+inside your current environment.
+Besides console scripts, the header (i.e. until _logger...) of this file can
+also be used as template for Python modules.
+
+Note: This skeleton file can be safely removed if not needed!
+"""
+from __future__ import division, print_function, absolute_import
+
+import argparse
+import sys
+import logging
+
+from pug.nlp import __version__
+
+__author__ = "Hobson Lane"
+__copyright__ = "Hobson Lane"
+__license__ = "none"
+
+_logger = logging.getLogger(__name__)
+
+
+def fib(n):
+    """
+    Fibonacci example 
