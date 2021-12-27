7fee56463dc669b5d278a1d5df812e85a8db11f4
jawking <126416933+jawking@users.noreply.github.com>
2021-12-27 12:23:14 +0000

    playlist

--- pug/nlp/stats.py
+++ pug/nlp/stats.py
@@ -33,4 +33,23 @@ def mcc_chi(mcc, num_samples):
 phi2chi = mcc_chi
 
 
-def random_split(df, first_portion=.1)
+def random_split(df, first_portion=.1):
+    """Randomly sample Dataframe rows. Typically for testset/training set cross-valication
+
+    random_split(pd.DataFrame())
+    """
+    mask = np.random.binomial(1, first_portion, size=len(df)).astype(bool)
+    return df[mask].copy(), df[~mask].copy()
+
+
+def safe_div(a, b, inf=INF):
+    """Safely divide by zero and inf and nan and produce floating point result
+    Numerical results are equivalent to `from __future__ import division`
+    Args:
+      a (float or int): numerator
+      b (float or int): denominator
+      inf (float or object): value to return in place of division by zero errors
+      none (float or object): value to return in place of TypeErrors (division by None)
+    Returns:
+      dividend:
+   
