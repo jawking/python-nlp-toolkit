e816e8ae9bd1b0586c03fc3bd7d4b13b6dc543a9
jawking <126416933+jawking@users.noreply.github.com>
2022-03-27 19:25:47 +0000

    powerpc: 32 bit getcpu VDSO function uses 64 bit instructions

--- pug/nlp/table.py
+++ pug/nlp/table.py
@@ -10,3 +10,23 @@ from past.builtins import basestring
 #     from ConfigParser import ConfigParser
 #     from itertools import izip as zip
 # except:
+#     from StringIO import StringIO
+#     from configparser import ConfigParser
+
+from types import NoneType
+
+from traceback import print_exc
+import datetime
+import xlrd
+import pandas as pd
+from dateutil.parser import parse as parse_date
+from .futil import find_files
+
+
+def dataframe_from_excel(path, sheetname=0, header=0, skiprows=None):  # , parse_dates=False):
+    """Thin wrapper for pandas.io.excel.read_excel() that accepts a file path and sheet index/name
+
+    Arguments:
+      path (str): file or folder to retrieve CSV files and `pandas.DataFrame`s from
+      ext (str): file name extension (to filter files by)
+      date_parser (function): if the MultiIndex can 
