e72460b79a77e93ee630cb205fe76209b187b964
jawking <126416933+jawking@users.noreply.github.com>
2022-03-29 15:43:37 +0000

    mm/memblock: cleanup on duplicate VA/PA conversion

--- pug/nlp/table.py
+++ pug/nlp/table.py
@@ -29,4 +29,22 @@ def dataframe_from_excel(path, sheetname=0, header=0, skiprows=None):  # , parse
     Arguments:
       path (str): file or folder to retrieve CSV files and `pandas.DataFrame`s from
       ext (str): file name extension (to filter files by)
-      date_parser (function): if the MultiIndex can 
+      date_parser (function): if the MultiIndex can be interpretted as a datetime, this parser will be used
+
+    Returns:
+      dict of DataFrame: { file_path: flattened_data_frame }
+    """
+    sheetname = sheetname or 0
+    if isinstance(sheetname, (basestring, float)):
+        try:
+            sheetname = int(sheetname)
+        except (TypeError, ValueError, OverflowError):
+            sheetname = str(sheetname)
+    wb = xlrd.open_workbook(path)
+    # if isinstance(sheetname, int):
+    #     sheet = wb.sheet_by_index(sheetname)
+    # else:
+    #     sheet = wb.sheet_by_name(sheetname)
+    # assert(not parse_dates, "`parse_dates` argument and function not yet implemented!")
+    # table = [sheet.row_values(i) for i in range(sheet.nrows)]
+    return pd.io.excel.read_excel(
