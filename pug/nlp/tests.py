fc40f38230c832efac74eae4ab2484d8e3d7e331
jawking <126416933+jawking@users.noreply.github.com>
2022-05-04 21:55:32 +0000

    Update read.me

--- pug/nlp/tests.py
+++ pug/nlp/tests.py
@@ -16,4 +16,19 @@ class NLPDocTest(TestCase):
             failure_count, test_count = doctest.testmod(module, raise_on_error=False, verbose=True)
             msg = "Ran {0} tests in {3} and {1} passed ({2} failed)".format(test_count, test_count-failure_count, failure_count, module.__file__)
             print msg
-           
+            if failure_count:
+                # print "Ignoring {0} doctest failures...".format(__file__)
+                self.fail(msg)
+            # return failure_count, test_count
+
+    def test_util(self):
+        self.test_module(util, optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+
+    def test_http(self):
+        self.test_module(http, optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+
+    def test_regex_patterns(self):
+        self.test_module(regex, optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+
+    def test_penn_treebank_tokenizer(self):
+        self.test_module(penn_treebank_tokenizer, optionflags=
