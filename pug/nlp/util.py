31b0aabf8810904fce98bc9eca706359e1161929
jawking <126416933+jawking@users.noreply.github.com>
2022-06-01 20:21:14 +0000

    Style changes

--- pug/nlp/util.py
+++ pug/nlp/util.py
@@ -13,4 +13,21 @@
 * Convert a sequence of sequences to a dictionary of sequences
 * Pierson correlation coefficient calculation
 * Parse a string into sentences or tokens
-* Table (list of l
+* Table (list of list) manipulation
+* make_time, make_date, quantize_datetime -- ignore portions of a datetime struct
+* ordinal_float, datetime_from_ordinal_float -- conversion between datetimes and float days
+* days_since    -- subract two date or datetime objects and return difference in days (float)
+
+'''
+from __future__ import division, print_function, absolute_import  # , unicode_literals
+# from future import standard_library
+# standard_library.install_aliases()  # noqa
+from builtins import next
+from builtins import map
+from builtins import zip
+from builtins import chr
+from builtins import range
+from builtins import object
+from builtins import str  # noqa
+from future.utils import viewitems
+from 
