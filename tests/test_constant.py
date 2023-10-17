7615e4a90b93506c4998b9529032d9c7660d8de4
jawking <126416933+jawking@users.noreply.github.com>
2023-10-17 08:59:01 +0000

    I18n: Update translation zh_TW (100%).

--- tests/test_constant.py
+++ tests/test_constant.py
@@ -4,4 +4,20 @@ import doctest
 
 import pug.nlp.constant
 
-from unittest import
+from unittest import TestCase
+
+
+class T(TestCase):
+    """Do-Nothing Test to ensure unittest doesnt ignore this file"""
+
+    def setUp(self):
+        pass
+
+    def test_doctests(self):
+        self.assertEqual(doctest.testmod(pug.nlp.constant, verbose=True).failed, 0)
+
+
+def load_tests(loader, tests, ignore):
+    """Run doctests for the clayton.nlp module"""
+    tests.addTests(doctest.DocTestSuite(pug.nlp.constant, optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
+    return tests
---
+++ tests/test_doctests.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+"""
+Uses the python unittest module to test this app with `python -m unittest pug.nlp`.
+"""
+from __future__ import division, print_function, absolute_import
+from future imp
