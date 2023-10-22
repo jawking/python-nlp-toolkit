b05db8ef18766245d064a5c7be8f7119924b7fae
jawking <126416933+jawking@users.noreply.github.com>
2023-10-22 15:00:24 +0000

    Release candidate.

---
+++ tests/test_skeleton.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from pug.nlp.skeleton import fib
+
+__author__ = "Hobson Lane"
+__copyright__ = "Hobson Lane"
+__license__ = "none"
+
+
+def te
