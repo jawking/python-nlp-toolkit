0fe5d880059946015faac27b1897dcce615b49ad
jawking <126416933+jawking@users.noreply.github.com>
2023-10-25 09:09:44 +0000

    format update

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -9,4 +9,9 @@ __copyright__ = "Hobson Lane"
 __license__ = "none"
 
 
-def te
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
