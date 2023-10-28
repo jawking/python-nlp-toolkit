3646c8489df76edb358398056e1c02e5ef37828a
jawking <126416933+jawking@users.noreply.github.com>
2023-10-28 16:58:23 +0000

    PM: devfreq: Always reflect a change of polling interval

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-# This script is meant to be ca
+# This script is meant to be called by the "install" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
+#
+# This script is taken from Scikit-Learn (http://scikit-learn.org/)
+#
+# THIS SCRIPT IS SUPPOSED TO BE AN EXAMPLE. MODIFY IT ACCORDING TO YOUR NEEDS!
+
+set -e
+
+if [[ "$DISTRIB" == "conda" ]]; then
+    # Deactivate the travis-provided virtual environment and setup a
+    # conda-based environment instead
+    deactivate
+
+    # Use the miniconda installer for faster download / install of conda
